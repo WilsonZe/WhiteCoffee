ff6929a10f8e5d188c447cc42c3f64db2e301dd7
WilsonZe <wilsonliaoz@gmail.com>
2017-10-30 22:01:59 +0800

    add user agent middleware

--- CoffeeSpider/CoffeeSpider/settings.py
+++ CoffeeSpider/CoffeeSpider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'CoffeeSpider.spiders'
 #USER_AGENT = 'CoffeeSpider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,13 +27,13 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'CoffeeSpider.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # 'CoffeeSpider.middlewares.MyCustomDownloaderMiddleware': 543,
+   'CoffeeSpider.middlewares.RandomUserAgentMiddleware': 200,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
@@ -88,3 +89,5 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+RANDOM_UA_TYPE = "firefox"
--- CoffeeSpider/CoffeeSpider/spiders/importnew.py
+++ CoffeeSpider/CoffeeSpider/spiders/importnew.py
@@ -5,7 +5,7 @@ import scrapy
 class ImportnewSpider(scrapy.Spider):
     name = 'importnew'
     allowed_domains = ['importnew.com']
-    start_urls = ['http://importnew.com/']
+    start_urls = ['http://www.importnew.com/all-posts']
 
     def parse(self, response):
         pass
